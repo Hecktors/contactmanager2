c441c08754f7f0e9ceeb3a2ec0be1c370cdb84dd
Hecktors <torsten.heckmann@gmx>
2018-10-20 14:28:41 +0200

    fixed brand link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <header>
       <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
         <div className="container">
-          <a href="/" className="navbar-brand">{branding}</a>
+          <Link to="/" className="navbar-brand">{branding}</Link>
           <div>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item"><Link to="/" className="nav-link"><i className="fas fa-home"></i> Home</Link></li>
